86bfd2d972965b59c7247a99c6f4f292560dbf4c
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-21 21:05:39 -0600

    Add files via upload

--- sistema-escolar-pro/js/firebase.js
+++ sistema-escolar-pro/js/firebase.js
@@ -1,8 +1,12 @@
 const firebaseConfig = {
-apiKey:"TU_API_KEY",
-authDomain:"TU_DOMINIO",
-projectId:"TU_PROJECT_ID"
-};
+    apiKey: "AIzaSyBA9DHJkUHm-iY7ZN8DULHrvzsnTHp6FHo",
+    authDomain: "sistema-escolar-pro-3ccd4.firebaseapp.com",
+    projectId: "sistema-escolar-pro-3ccd4",
+    storageBucket: "sistema-escolar-pro-3ccd4.firebasestorage.app",
+    messagingSenderId: "991502018784",
+    appId: "1:991502018784:web:02defd9d4a91e76b31c885"
+  };
+  
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
